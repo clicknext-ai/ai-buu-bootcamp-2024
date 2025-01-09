b4f6a3821ac0ba4292f12cb4d304397604d69e0d
65160271 <153811190+65160271@users.noreply.github.com>
2025-01-09 23:15:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -106,4 +106,4 @@ def handle_message(event: MessageEvent):
 if __name__ == "__main__":
     uvicorn.run("main:app",
                 port=8000,
-                host="127.0.0.1")
+                host="0.0.0.0")
